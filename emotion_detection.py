340a94d53df74fdf9d2807d78168cae0467f6007
Vanildo Vanni <vanildov@br.ibm.com>
2025-05-13 11:19:31 -0300

    feat(emotion_detection): parse and format emotion detection response
    Add JSON parsing to process the API response and format it into a structured dictionary with individual emotions and the dominant emotion. This improves the usability of the response data for downstream processing.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -8,10 +9,24 @@ def emotion_detector(text_to_analyse: str) -> str:
   response = requests.post(URL, json=input, headers=HEADERS)
   
   if response.status_code == 200:
-    res = response.text
-    return res
-  else:
-    return ''
+    res = json.loads(response.text)
+
+    emotions = {
+      'anger': res[0]['emotion']['anger'],
+      'disgust': res[0]['emotion']['disgust'],
+      'fear': res[0]['emotion']['fear'],
+      'joy': res[0]['emotion']['joy'],
+      'sadness': res[0]['emotion']['sadness'],
+    }
+
+    formatted_res = {
+      **emotions,
+      'dominant_emotion': sorted(emotions.items(), key=lambda x: x[1], reverse=True)[0][0]
+    }
+
+  return formatted_res
+  # else:
+  #   return ''
 
 
 if __name__ == '__main__':
